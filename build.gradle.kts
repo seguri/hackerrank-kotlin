cff1622dd9a5e472fc7e1f79aa9263f018ae5f09
Marco Seguri <seguri@users.noreply.github.com>
2024-09-13 18:31:52 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+    id("com.diffplug.spotless") version "7.0.0.BETA2"
+}
+
+group = "name.seguri.kotlin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.26.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+spotless {
+    java {
+        googleJavaFormat().reorderImports(true).formatJavadoc(true)
+    }
+    kotlin {
+        // version, style and all configurations here are optional
+        ktfmt("0.52").googleStyle().configure {
+            it.setRemoveUnusedImports(true)
+            it.setManageTrailingCommas(true)
+        }
+    }
+}
