c4bb17c3b3c3c78117fc6531825cbd5f55fb12ea
Marco Seguri <seguri@users.noreply.github.com>
2024-09-23 18:37:36 +0200

    fix(leetcode-110): TreeNode builder implementation

--- src/main/java/name/seguri/java/leetcode/_110/TreeNode.java
+++ src/main/java/name/seguri/java/leetcode/_110/TreeNode.java
@@ -1,5 +1,10 @@
 package name.seguri.java.leetcode._110;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringJoiner;
+import java.util.function.Consumer;
+
 class TreeNode {
   int val;
   TreeNode left;
@@ -11,17 +16,52 @@ class TreeNode {
 
   /** Builds a tree from a level-ordered array */
   static TreeNode of(Integer... values) {
-    return of(0, values);
-  }
-
-  private static TreeNode of(int i, Integer... values) {
-    if (values == null || values.length == 0 || i >= values.length || values[i] == null) {
+    if (values == null || values.length == 0) {
       return null;
     }
 
-    var node = new TreeNode(values[i]);
-    node.left = of(2 * i + 1, values);
-    node.right = of(2 * i + 2, values);
-    return node;
+    var root = new TreeNode(values[0]);
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.add(root);
+    var i = 1;
+    while (!queue.isEmpty()) {
+      var curr = queue.remove();
+
+      if (i < values.length && values[i] != null) {
+        curr.left = new TreeNode(values[i]);
+        queue.add(curr.left);
+      }
+      i++;
+
+      if (i < values.length && values[i] != null) {
+        curr.right = new TreeNode(values[i]);
+        queue.add(curr.right);
+      }
+      i++;
+    }
+
+    return root;
+  }
+
+  @Override
+  public String toString() {
+    var sj = new StringJoiner(", ", "[", "]");
+    levelOrderTraversal(this, val -> sj.add(String.valueOf(val)));
+    return sj.toString();
+  }
+
+  private void levelOrderTraversal(TreeNode root, Consumer<Integer> consumer) {
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      var node = queue.remove();
+      consumer.accept(node.val);
+      if (node.left != null) {
+        queue.add(node.left);
+      }
+      if (node.right != null) {
+        queue.add(node.right);
+      }
+    }
   }
 }
