32fb153e1f90ca3d01f98ee4911c6f2910874b29
Marco Seguri <seguri@users.noreply.github.com>
2024-09-26 11:27:50 +0200

    chore: update formatting rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,32 @@
 plugins {
-    kotlin("jvm") version "2.0.10"
-    id("com.diffplug.spotless") version "7.0.0.BETA2"
+  kotlin("jvm") version "2.0.10"
+  id("com.diffplug.spotless") version "7.0.0.BETA2"
 }
 
 group = "name.seguri.kotlin"
+
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    testImplementation("org.assertj:assertj-core:3.26.3")
-    testImplementation("org.mockito:mockito-core:5.13.0")
+  testImplementation(kotlin("test"))
+  testImplementation("org.assertj:assertj-core:3.26.3")
+  testImplementation("org.mockito:mockito-core:5.13.0")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
-}
+tasks.test { useJUnitPlatform() }
+
+kotlin { jvmToolchain(21) }
 
 spotless {
-    java {
-        googleJavaFormat().reorderImports(true).formatJavadoc(true)
+  java { googleJavaFormat().reorderImports(true).formatJavadoc(true) }
+  kotlin {
+    target("**/*.kt", "**/*.kts")
+    // version, style and all configurations here are optional
+    ktfmt("0.52").googleStyle().configure {
+      it.setRemoveUnusedImports(true)
+      it.setManageTrailingCommas(true)
     }
-    kotlin {
-        // version, style and all configurations here are optional
-        ktfmt("0.52").googleStyle().configure {
-            it.setRemoveUnusedImports(true)
-            it.setManageTrailingCommas(true)
-        }
-    }
+  }
 }
