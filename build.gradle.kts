092776b67c7e9f322ff09bcb96cc3856859e4813
Marco Seguri <seguri@users.noreply.github.com>
2025-10-14 10:32:35 +0200

    chore: update project dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import java.net.URI
 import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
-  kotlin("jvm") version "2.1.10"
-  id("com.diffplug.spotless") version "7.0.2"
-  id("org.jetbrains.dokka") version "2.0.0"
+  kotlin("jvm") version "2.3.0"
+  id("com.diffplug.spotless") version "8.1.0"
+  id("org.jetbrains.dokka") version "2.1.0"
 }
 
 group = "name.seguri.kotlin"
@@ -15,9 +15,9 @@ repositories { mavenCentral() }
 
 dependencies {
   testImplementation(kotlin("test"))
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.0")
-  testImplementation("org.assertj:assertj-core:3.27.3")
-  testImplementation("org.mockito:mockito-core:5.16.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.1")
+  testImplementation("org.assertj:assertj-core:3.27.6")
+  testImplementation("org.mockito:mockito-core:5.21.0")
 }
 
 tasks.withType<Test> { useJUnitPlatform() }
@@ -27,9 +27,9 @@ java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 kotlin { jvmToolchain(21) }
 
 spotless {
-  java { googleJavaFormat().reorderImports(true).formatJavadoc(true) }
-  kotlin { ktfmt("0.54").googleStyle().configure { it.setRemoveUnusedImports(true) } }
-  kotlinGradle { ktfmt("0.54") }
+  java { googleJavaFormat("1.33.0").reorderImports(true).formatJavadoc(true) }
+  kotlin { ktfmt("0.60").googleStyle().configure { it.setRemoveUnusedImports(true) } }
+  kotlinGradle { ktfmt("0.60") }
 }
 
 tasks.withType<DokkaTask>().configureEach {
@@ -42,7 +42,8 @@ tasks.withType<DokkaTask>().configureEach {
       sourceLink {
         localDirectory.set(file("src/main/java"))
         remoteUrl.set(
-            URI("https://github.com/seguri/hackerrank-kotlin/tree/main/src/main/java").toURL())
+            URI("https://github.com/seguri/hackerrank-kotlin/tree/main/src/main/java").toURL()
+        )
         remoteLineSuffix.set("#L")
       }
 
@@ -51,7 +52,8 @@ tasks.withType<DokkaTask>().configureEach {
       sourceLink {
         localDirectory.set(file("src/main/kotlin"))
         remoteUrl.set(
-            URI("https://github.com/seguri/hackerrank-kotlin/tree/main/src/main/kotlin").toURL())
+            URI("https://github.com/seguri/hackerrank-kotlin/tree/main/src/main/kotlin").toURL()
+        )
         remoteLineSuffix.set("#L")
       }
     }
